287918ebe94f55f9893436fb29b6bc360fd14d01
nrayner73 <136016752+nrayner73@users.noreply.github.com>
2023-12-21 12:09:49 -0700

    first commit

---
+++ Gatekeeper/Program.cs
@@ -0,0 +1,76 @@
+using Gatekeeper.Components;
+using Gatekeeper.Interfaces;
+using Gatekeeper.Models;
+using Gatekeeper.Services;
+using Microsoft.EntityFrameworkCore;
+using Gatekeeper.DataServices.Lookups;
+using Gatekeeper.Interfaces.Lookups;
+using Gatekeeper.Models.Lookups;
+using System.Net.NetworkInformation;
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Authentication.Negotiate;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+
+// Add session 
+builder.Services.AddBlazoredSessionStorage();
+
+// Gatekeeper Services
+builder.Services.AddDbContextFactory<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("gkConnectionString")));
+
+builder.Services.AddDbContextFactory<LookupDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("gkConnectionString")));
+
+
+
+builder.Services.AddScoped<IRequestfileService, RequestfileService>();
+builder.Services.AddScoped<IPersonService, PersonService>();
+
+//Lookup
+builder.Services.AddScoped<ILkRequesttypeService, LkRequesttypeService>();
+builder.Services.AddScoped<ILkRequestStateService, LkRequestStateService>();
+builder.Services.AddScoped<ILkProcessingdeficiencyService, LkProcessingdeficiencyService>();
+builder.Services.AddScoped<ILkRecorddeliverymethodService, LkRecorddeliverymethodService>();
+builder.Services.AddScoped<IAnalystsService, AnalystsService>();
+builder.Services.AddScoped<ISearchrequestfileService, SearchrequestfileService>();
+
+//View
+builder.Services.AddScoped<IPersonnameService,PersonnameService>();
+builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IContactService, ContactService>();
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+    .AddNegotiate();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();

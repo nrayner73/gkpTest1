455308fe285a69eda926dbd57ccad7c480ebb353
nuu rayner <pol11067@calgarypolice.ca>
2024-02-18 21:35:54 -0700

    check user access

---
+++ Gatekeeper/Models/Lookups/UserInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace Gatekeeper.Models.Lookups
+{
+    public class UserInfo
+    {
+        
+        public string? gbUserReg { get; set; }
+        public bool? canaccess { get; set; }
+
+        public event Action OnChange;
+        public void SetLogin(Requestfile? requestfile)
+        {
+            
+
+
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged()
+        {
+            OnChange?.Invoke();
+        }
+    }
+}

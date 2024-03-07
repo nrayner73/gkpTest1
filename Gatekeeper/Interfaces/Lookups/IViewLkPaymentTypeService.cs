85cbde57084ad8bb6682a736576365306c0c9f70
Beth Anglo <banglo@calgarypolice.ca>
2024-03-07 13:56:47 -0700

    Payment type management page
    relates to git 43

---
+++ Gatekeeper/Interfaces/Lookups/IViewLkPaymentTypeService.cs
@@ -0,0 +1,11 @@
+﻿using Gatekeeper.Models;
+using Gatekeeper.Models.Lookups;
+
+namespace Gatekeeper.Interfaces.Lookups
+{
+    public interface IViewLkPaymentTypeService
+    {
+        List<LkPaymentTypeView> GetLkPaymenttypeList();
+       
+    }
+}

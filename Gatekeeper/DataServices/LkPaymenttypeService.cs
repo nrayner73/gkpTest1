b0f0d2c5b015a97dd18811daa63ffa9a3e8f47b3
Beth Anglo <banglo@calgarypolice.ca>
2024-04-11 13:10:36 -0600

    Add management page for Record Delivery Methods; edit Payment type page
    relates to git 52, 43

--- Gatekeeper/DataServices/LkPaymenttypeService.cs
+++ Gatekeeper/DataServices/LkPaymenttypeService.cs
@@ -27,6 +27,20 @@ namespace Gatekeeper.DataServices
 
         public async Task<LkPaymenttype> CreateLkPaymentType(LkPaymenttype lkpaymenttype)
         {
+            lkpaymenttype.Status = "1";
+
+            var lastRecord = await _context?.LkPaymenttypes.OrderByDescending(x => x.Sortby)
+                .FirstOrDefaultAsync();
+
+            if (lastRecord is not null)
+            {
+                lkpaymenttype.Sortby = lastRecord.Sortby + 1;
+            }
+            else
+            {
+                lkpaymenttype.Sortby = 1; //1st Request State record
+            }
+
             _context.LkPaymenttypes.Add(lkpaymenttype);
             await _context.SaveChangesAsync();
             return lkpaymenttype;
@@ -39,7 +53,8 @@ namespace Gatekeeper.DataServices
 
         public async Task DeleteLkPaymentType(LkPaymenttype lkpaymenttype)
         {
-            _context.LkPaymenttypes.Remove(lkpaymenttype);
+            lkpaymenttype.Status = "0"; 
+            _context.LkPaymenttypes.Update(lkpaymenttype);
             await _context.SaveChangesAsync();
         }
 
---
+++ Gatekeeper/Interfaces/ISearchLkRecordDeliveryMethodService.cs
@@ -0,0 +1,10 @@
+﻿using Gatekeeper.Models;
+using Gatekeeper.Models.Lookups;
+
+namespace Gatekeeper.Interfaces.Lookups
+{
+    public interface ISearchLkRecordDeliveryMethodService
+    {
+        List<SearchLkRecordDeliveryMethod> GetLkRecordDeliveryMethodList();
+    }
+}
